814861e140e05f27779c98738e32c8a81a72141a
Rome Bell <romebell@me.com>
2019-04-01 13:55:44 -0700

    Add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+let path = require('path');
+let webpack = require('webpack');
+
+let plugins = [];
+let devPlugins = [];
+
+let prodPlugins = [
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+    }
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: true
+    }
+  })
+];
+
+plugins = plugins.concat(
+  process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
+);
+
+module.exports = {
+  context: __dirname,
+  entry: "./public/js/app.js",
+  output: {
+    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+    filename: "bundle.js"
+  },
+  plugins: plugins,
+  devtool: 'source-map',
+  resolve: {
+    extensions: [".js", ".jsx", "*"]
+  }
+};
