53373d6e93bb17aa6c7e1e0aff5f9d9251e6a06d
Rome Bell <romebell@me.com>
2019-04-01 15:03:25 -0700

    Add packages and ignore node_modules and yarn.lock files

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ plugins = plugins.concat(
 
 module.exports = {
   context: __dirname,
-  entry: "./public/js/app.js",
+  entry: "./app.js",
   output: {
     path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
     filename: "bundle.js"
