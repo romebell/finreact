13585f57c87dffc2055a057aa7c01ecd9c3e7dbb
Rome Bell <romebell@me.com>
2019-06-12 09:57:29 -0700

    Add more width to tweet container and update text for tweet stream

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -2,28 +2,44 @@ let socket = io();
 let tweetArray = [];
 let index = 0;
 
+// document.getElementsById('alert').addEventListener('click', function() {
+//   alert('hello');
+// });
+
+console.log(document.getElementById("alert"));
+
+function myFunction() {
+  alert ("Hello World!");
+}
+
 socket.on('tweet', function (tweet) {
   
   // console.log('connected inside of tweet');
     
-    let currentTweet = tweet.tweet;
-    console.log(currentTweet);
-    
-    let tweetContent = $('ul.tweets');
-    let screenName = '<div id="name">' + currentTweet.name + '</div>';
-    let username = '<div id="username">' + currentTweet.userScreenName + '</div>';
-    let userImage = `<img id="prof-pic" src="${currentTweet.userImage}"/>`;
-
-
-    tweetContent.prepend(
-      '<li>' 
-        + '<div id="prof-info">' 
-          + userImage 
-          + '<div id="prof-names">' 
-            + screenName 
-            + username 
-          + '</div>' 
-        + '</div>' + currentTweet.text + 
-      '</li>');
+  let currentTweet = tweet.tweet;
+  // console.log(currentTweet);
+  
+  let tweetContent = $('ul.tweets');
+  let screenName = '<div id="name">' + currentTweet.name + '</div>';
+  let username = '<div id="username">' + currentTweet.userScreenName + '</div>';
+  let userImage = `<img id="prof-pic" src="${currentTweet.userImage}"/>`;
+  
+  
+  tweetContent.prepend(
+    '<li id="alert">' 
+    + '<div id="prof-info">' 
+    + userImage 
+    + '<div id="prof-names">' 
+    + screenName 
+    + username 
+    + '</div>' 
+    + '</div>' + currentTweet.text + 
+    '</li>');
     socket.emit('tweetArray', tweetArray);
-});
+    
+  });
+  
+  
+  // function showProfile () {
+//   alert('Hello');
+// }
