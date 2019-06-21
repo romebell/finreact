0bdf1acbdc092829c1350f694086a25cd6658e50
Rome Bell <romebell@me.com>
2019-06-21 15:17:52 -0700

    Update production keys

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,6 +1,6 @@
 module.exports = {
-  ACCESS_TOKEN: process.env.ACCESS_TOKEN,
-  ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET,
-  CONSUMER_KEY: process.env.CONSUMER_KEY,
-  CONSUMER_SECRET: process.env.CONSUMER_SECRET
+  access_token: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET
 }
